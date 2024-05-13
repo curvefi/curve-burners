3da7a82a6b81ccf9be959d4de8589db5e08d5ad3
Roman Agureev <Agureev.RS@phystech.edu>
2024-05-13 22:25:10 +0300

    chore: update deploy script

--- scripts/deploy.py
+++ scripts/deploy.py
@@ -6,14 +6,21 @@ from getpass import getpass
 from eth_account import account
 
 
+chain = "gnosis"  # ALTER
 TARGET = "0xaBEf652195F98A91E490f047A5006B71c85f058d"  # ALTER: crvUSD
 WETH = "0xe91D153E0b41518A2Ce8Dd3D7944Fa863463a97d"  # ALTER: wrapped native coin
 ADMIN = "0x71F718D3e4d1449D1502A6A7595eb84eBcCB1683"  # ALTER
 EMERGENCY_ADMIN = "0x71F718D3e4d1449D1502A6A7595eb84eBcCB1683"  # ALTER
 
-BURNER = "XYZ"  # ALTER
+BURNER = "CowSwap"  # ALTER
 
 NETWORK = f"https://rpc.gnosischain.com"  # ALTER
+
+EMPTY_COMPENSATION = (0, 0, 0, 0, False)
+EMPTY_HOOK_INPUT = (0, 0, b"")
+
+MIN_BRIDGE_AMOUNT = 100 * 10 ** 18  # ALTER: 100 crvUSD
+ETHEREUM_FEE_DESTINATION = "0xeCb456EA5365865EbAb8a2661B0c503410e9B347"  # FeeDistributor on Ethereum
 
 
 def deploy():
@@ -21,10 +28,11 @@ def deploy():
     # fee_collector = boa.load_partial("contracts/FeeCollector.vy").at("")
     print(f"FeeCollector: {fee_collector.address}")
 
-    hooker = boa.load("contracts/Hooker.vy", fee_collector)
+    hooker_inputs = deploy_hooks()
+    hooker = boa.load("contracts/hooks/Hooker.vy", fee_collector, *hooker_inputs)
     # hooker = boa.load_partial("contracts/Hooker.vy").at("")
     print(f"Hooker: {hooker.address}")
-    # fee_collector.set_hooker(hooker)
+    fee_collector.set_hooker(hooker)
 
     burner = deploy_burner(fee_collector)
     print(f"Burner: {burner.address}")
@@ -46,6 +54,35 @@ def deploy_burner(fee_collector):
                         )
         # return boa.load_partial("contracts/burners/CowSwapBurner.vy").at("")
     raise ValueError("Burner not specified")
+
+
+def deploy_hooks():
+    initial_oth, initial_oth_inputs, initial_hooks = [], [], []
+
+    # Custom hooks
+    if chain == "gnosis":
+        # bridger = boa.load("contracts/hooks/gnosis/GnosisBridger.vy")
+        bridger = boa.load_partial("contracts/hooks/gnosis/GnosisBridger.vy").at("0xc4AA2fB0A8837a06d296b1c0DE1990E401659449")
+
+    # Bridger
+    if chain != "ethereum":
+        target = boa.load_partial("contracts/testing/ERC20Mock.vy").at(TARGET)
+        initial_oth.append((TARGET, target.approve.prepare_calldata(bridger, 2 ** 256 - 1), EMPTY_COMPENSATION, False))
+        initial_oth_inputs.append(EMPTY_HOOK_INPUT)
+        initial_hooks.append(
+            (
+                bridger,
+                bridger.bridge.prepare_calldata(TARGET, ETHEREUM_FEE_DESTINATION, 2 ** 256 - 1, MIN_BRIDGE_AMOUNT),
+                EMPTY_COMPENSATION,
+                True,
+            )
+        )
+
+    # FeeDistributor
+    else:
+        pass
+
+    return initial_oth, initial_oth_inputs, initial_hooks
 
 
 def account_load(fname):
