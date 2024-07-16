7f9492ed81abacdd8cb8408016ce48f0be9cb049
Alberto <albicento.ac@gmail.com>
2024-07-16 18:00:41 +0200

    test: test cases placeholders

---
+++ tests/fee_splitter/test_fee_splitter.py
@@ -0,0 +1,59 @@
+import boa
+import pytest
+
+def test_constructor():
+    pass
+
+def test_constructor_zero_address():
+    pass
+
+def test_constructor_more_than_100():
+    pass
+
+def test_claim_controller_fees_expected():
+    pass
+
+def test_set_weights_expected():
+    pass
+
+def test_set_weights_unauthorized():
+    pass
+
+def test_set_weights_more_than_100():
+    pass
+
+def test_set_controllers_expected():
+    pass
+
+def test_set_controllers_unauthorized():
+    pass
+
+def test_set_distribution_receiver_expected():
+    pass
+
+def test_set_distribution_receiver_unauthorized():
+    pass
+
+def test_set_distribution_receiver_zero_address():
+    pass
+
+def test_set_incentives_receiver_expected():
+    pass
+
+def test_set_incentives_receiver_unauthorized():
+    pass
+
+def test_set_incentives_receiver_zero_address():
+    pass
+
+def test_set_owner_expected():
+    pass
+
+def test_set_owner_unauthorized():
+    pass
+
+def test_set_owner_zero_address():
+    pass
+
+def test_incentives_weight_expected():
+    pass
